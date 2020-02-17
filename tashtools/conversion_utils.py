af1f252d6777d77bcd15d1583f16c9d7867d32d2
Natalia Orlova <nataliao@alleninstitute.org>
2020-02-17 15:33:33 -0800

    to_16bit changed input to be numpy array vs file path

--- tashtools/conversion_utils.py
+++ tashtools/conversion_utils.py
@@ -1,22 +1,11 @@
-from . import read_tiff, read_h5
 import numpy as np
 
 
-def to_16bit(path):
-	if ".tiff" or ".tif" in path:
-		data  = read_tiff(path)
-	elif ".h5" in path:
-		data = read_h5(path)
-	else:
-		raise ValueError('Input file has to be *.tiff or *.hdf5')
-
+def to_16bit(data):
 	data_min = np.min(data)
-
 	if data_min < 0:
 		data -= data_min
-
 	data_max = np.max(data)
-
 	if data_max > 65355 :
 		raise ValueError('Cancelling. Data needs to eb re-scaled first to avoid higher values truncation.')
 	else:
