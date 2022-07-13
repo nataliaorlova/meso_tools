247c54be84c2f3519bc1ea4de43fbed94dc6fc68
Natalia <nataliao@alleninstitute.org>
2022-07-13 12:39:00 -0700

    fixed init

--- meso_tools/__init__.py
+++ meso_tools/__init__.py
@@ -2,7 +2,7 @@ from . io_utils import LimsApi, read_tiff as read_tiff
 from . io_utils import write_tiff as write_tiff
 from . io_utils import read_h5 as read_h5
 from . io_utils import write_h5 as write_h5
-from . io_utils import LimsApi as LimsApi
+# from . io_utils import LimsApi as LimsApi
 from . io_utils import load_motion_corrected_movie as load_motion_corrected_movie
 from . conversion_utils import to_16bit as to_16bit
 from . image_tools import get_pixel_hist2d as get_pixel_hist2d
