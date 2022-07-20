35e67d3fb47db616a09fc431c2df0dfe85fe7b56
Natalia <nataliao@alleninstitute.org>
2022-07-19 23:25:30 -0700

    typo in init align phase

--- meso_tools/__init__.py
+++ meso_tools/__init__.py
@@ -2,13 +2,13 @@ from . io_utils import read_tiff as read_tiff
 from . io_utils import write_tiff as write_tiff
 from . io_utils import read_h5 as read_h5
 from . io_utils import write_h5 as write_h5
-# from . io_utils import LimsApi as LimsApi
+from . io_utils import LimsApi as LimsApi
 from . io_utils import load_motion_corrected_movie as load_motion_corrected_movie
 from . conversion_utils import to_16bit as to_16bit
 from . image_tools import get_pixel_hist2d as get_pixel_hist2d
 from . image_tools import im_plot as im_plot
 from . image_tools import plot_all_colormaps as plot_all_colormaps
 from . image_tools import average_intensity as average_intensity
-from . image_tools import align_phase as aling_phase
+from . image_tools import align_phase as align_phase
 from . image_tools import align_phase_stack as align_phase_stack
 from . image_tools import average_n as average_n
