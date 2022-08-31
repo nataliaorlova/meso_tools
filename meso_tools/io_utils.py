5300bebe9aa3de2a9c601014ad1d9a076bcedcd4
Natalia <nataliao@alleninstitute.org>
2022-08-31 12:05:31 -0700

    more docs

--- meso_tools/io_utils.py
+++ meso_tools/io_utils.py
@@ -1,8 +1,7 @@
 ## this file has input/output related functions
 ### reading/writing tiff, hdf5s, reading metadata
 
-import json
-from typing import Any
+from typing import Any, Union
 import tifffile
 import h5py
 import pandas as pd
@@ -11,19 +10,19 @@ from allensdk.internal.api import PostgresQueryMixin
 
 def read_tiff(path_to_tiff : str, page_num : int = None) -> np.Array:
     """
-    Reads either entire tiff file, or if page_num is given, only those pages are returned
+    Reads either entire tiff file, or if page_num is given, only those pages or pages range
     Parameters
     -------
     path_to_tiff : str
         local path to the tiff file
     page_num : int or [int, int]
-        number of pages to read, if none provided will atempt to read entire tiff file. 
+        number of pages to read, if none provided will atempt to read entire tiff file.
         Will limlit to 5000 if tiff has more that 5000 pages.
         if list of 2 ints, a range, - will read pages from the range
     Returns
     -------
     tiff_array : np.array
-        3D numpy array representing timeseries that was read
+        numpy array representing timeseries that was read
     """
     with tifffile.TiffFile(path_to_tiff, mode ='rb') as tiff:
         if page_num: 
@@ -32,10 +31,10 @@ def read_tiff(path_to_tiff : str, page_num : int = None) -> np.Array:
                 tiff_array = tiff.asarray(range(page_num[0], page_num[1]))
             else:
                 tiff_array = tiff.asarray(range(0, page_num))
-        else: # number of pages is not provided: 
+        else: # number of pages is not provided:
             if len(tiff.pages) >=5000:
-                print(f"This timeseries has more than 5000 frames to not overload RAM, we will only read 5000 first pages.")
-                print(f"To read more pages, in case large amount of RAM is available, provide number of pages to read by calling read_tiff(path_to_tiff, page_num=value)")
+                print("This timeseries has more than 5000 frames to not overload RAM, we will only read 5000 first pages.")
+                print("To read more pages, in case large amount of RAM is available, provide number of pages to read by calling read_tiff(path_to_tiff, page_num=value)")
                 page_num = 5000
                 tiff_array = tiff.asarray(range(0, page_num))
             else:
@@ -59,89 +58,148 @@ def write_tiff(path_to_tiff : str, data : np.Array) -> None:
     return
 
 def read_h5(path_to_h5 : str, field : str) -> Any:
-
     """
     Read a field from hdf5 file, wrapping h5py
     Parameters
     -------
     path_to_h5: str
         path to hdf5 file
-    field : str 
+    field : str
         datafield to read
     Returns
-    ------- 
+    -------
     data : any
         data contained in the given field of the h5 file
-    """
-    
+    """  
     with h5py.File(path_to_h5, "r") as h5_file:
         fields = h5_file.keys()
         if field not in fields:
             print("Specified field is not in h5 file")
             return None
-        else:
-            data = h5_file[field][()]
+        data = h5_file[field][()]
     return data
 
-def write_h5(path, h5_data):
-    with h5py.File(path, 'w') as f:
-        f.create_dataset('data', data=h5_data)
+def write_h5(path : str, h5_data : Any) -> None:
+    """
+    Writes to disk file in hdf5 format, wrapper for h5py
+    Parameters
+    ----------
+    path : str
+        Absolute path to file to eb saved
+    h5_data : Any
+        Data to write to file
+    """
+    with h5py.File(path, 'w') as h5_file:
+        h5_file.create_dataset('data', data=h5_data)
     return
 
-def read_si_metadata(path_to_tiff : str) -> json:
+def read_si_metadata(path_to_tiff : str) -> dict:
     """
-    function to read scnaimage metadata in full
-    path: path to tiff file
-    returns: dict w metadata
+    Read ScanImage metadata
+    Parameters
+    ----------
+    path_to_tiff : str
+        tiff file whose metadata needs to be read
+
+    Returns
+    -------
+    meta_data : dict
+        disctionary with metadata
     """
     meta_data = tifffile.read_scanimage_metadata(open(path_to_tiff, 'rb'))
     return meta_data
 
-def get_roi_data(path_to_tiff):
+def get_roi_data(path_to_tiff : str) -> dict:
     """
-    function to read scnaimage metadata's ROI structure part
-    path: path to tiff file
-    returns: dict w metadata
-    """
+    Read scanimage metadata's ROI structure part
+    Parameters
+    ----------
+    path_to_tiff : str
+        Path to tiff file
+    Returns
+    -------
+    meta_data : dict
+        dict w metadata
+    """    
     meta_data = tifffile.read_scanimage_metadata(open(path_to_tiff, 'rb'))
     return meta_data[1]
 
-def load_motion_corrected_movie(filepath, pages=None):
-    """load motion correctionmovie : whole or some pages
-    filepath :  str : absolute path to teh hdf5 file with movie
-    pages :  int : number of pages to load, if given
-    return : loaded movie as a 3D numpy array
+def load_motion_corrected_movie(filepath : str, page_num : int =None) -> np.Array:
     """
+    Load motion corrected movie as numpy array; whole or some pages
+    Parameters
+    ----------
+    filepath : str
+        absolute path to teh hdf5 file with movie
+    page_num : int, optional
+        number of pages to load, if given
+
+    Returns
+    -------
+    motion_corrected_movie : np.Array
+        loaded movie as a 3D numpy array
+    """    
     with h5py.File(filepath, 'r') as motion_corrected_movie_file:
-        if not pages:        
+        if not page_num:        
             motion_corrected_movie = motion_corrected_movie_file['data']
-        elif pages > 0:
-            motion_corrected_movie = motion_corrected_movie_file['data'][:pages]
+        elif page_num > 0:
+            motion_corrected_movie = motion_corrected_movie_file['data'][:page_num]
         else: 
-            motion_corrected_movie = motion_corrected_movie_file['data'][pages:]
+            motion_corrected_movie = motion_corrected_movie_file['data'][page_num:]
     return motion_corrected_movie
 
 class LimsApi():
-    def __init__(self, lims_credentials):
+    """
+    Class with simple queries to LIMS database, must have access to the credentials and read it prior to instantiating the class
+    """
+    def __init__(self, lims_credentials : dict):
+        """
+
+        Parameters
+        ----------
+        lims_credentials : dict
+            disctionary with database access credentials
+        """
         self.lims_db = PostgresQueryMixin(
             dbname=lims_credentials['dbname'], user=lims_credentials['user'],
             host=lims_credentials['host'], password=lims_credentials['password'],
             port=lims_credentials['port'])
         
-    def get_exp_folder(self, exp_id):
-        """get path to the storage directory for given experiment id
+    def get_exp_folder(self, exp_id : int) -> Union[str, None]:      
         """
+        Get path to the storage directory for given experiment id, via a direct query to LIMS
+        Parameters
+        ----------
+        exp_id : int
+            Experiment ID assigned in LIMS
+        Returns
+        -------
+        Union[str, None]
+            path to experimental folder, if exists, or None
+        """        
         query = f"""SELECT
                     oe.storage_directory as experiment_folder
                     FROM ophys_experiments oe
                     WHERE oe.id={exp_id}"""
         exp_folder_pd = pd.read_sql(query, self.lims_db.get_connection())
         if len(exp_folder_pd) != 0:
-            return exp_folder_pd.experiment_folder[0]
-        else: print(f"can't find folder for experiment {exp_id}")
+            path = exp_folder_pd.experiment_folder[0]
+            return path
+        else: print(f"Can't find folder for experiment {exp_id}")
             
-    def get_motion_corrected_stack(self, exp_id):
-        """get path to the motion corrected stack for given experiment id
+    def get_motion_corrected_stack(self, exp_id : int) -> Union[str, None]:
+        """
+        Get path to the motion corrected stack for given experiment ID via a direct query to LIMS
+
+        Parameters
+        ----------
+        exp_id : int
+            Experiment ID assigned in LIMS
+
+        Returns
+        -------
+        Union[str, None]
+            path to motion corrected stack, if exists, or None
         """
         query = f"""SELECT wkf.storage_directory || wkf.filename AS mc_stack_file
                     FROM ophys_experiments oe
@@ -157,13 +215,25 @@ class LimsApi():
             return mc_file.mc_stack_file[0]
         else: print(f"can't find motion corrected stack for experiment {exp_id}")
     
-    def get_all_table_columns(self, table_name):
-        """get all columns in given LIMS table
+    def get_all_table_columns(self, table_name : str) -> Union[list, None]:
         """
+        Get all columns in given LIMS table via a direct query to LIMS
+        Parameters
+        ----------
+        table_name : str
+            Name of the LIMS table
+        Returns
+        -------
+        Union[str, None]
+            list of strings with names of all columns, or None
+        """        
         query = (f"""SELECT * FROM {table_name} WHERE 1=0""")
-        table_columns = pd.read_sql(query, self.lims_db.get_connection())
-        return table_columns.columns.values
-
+        table_columns = list(pd.read_sql(query, self.lims_db.get_connection()).keys())
+        if len(table_columns) == 0:
+            print(f"table {table_name} has no columns")
+            return None
+        return table_columns
+        
     def get_all_distinct_values_in_column(self, table, column):
         """
         Get all distinct values in column/table via a direct query to LIMS
