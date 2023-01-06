0c1da4d63062f8b8c19c40d42575cb402a680de2
Natalia <nataliao@alleninstitute.org>
2023-01-06 13:06:33 -0800

    basic backend done

--- meso_tools/laser_mon.py
+++ meso_tools/laser_mon.py
@@ -8,19 +8,21 @@ class RigolAPI():
     def __init__(self):
         """
         Here we will configure the backend, find Rigol device, get it's address and open it for communication
+        Make sure only one Rigol Oscilloscope is connected to given host 
         """
         rm = visa.ResourceManager()
-        addresses = rm.list_resources()
+        addresses = rm.list_opened_resources()
         # find rigol device - should be on USB0:
         for address in addresses:
-            device = rm.open_resource(address)
-            name = device.query("*IDN?")
-            if 'DS7034' in name:
-                self.rigol = device
-                print(f"Found Rigol as {name}\nAt address {address}")
-                break
+            if 'USB' in address:
+                device = rm.open_resource(address)
+                name = device.query("*IDN?")
+                if 'RIGOL' in name:
+                    self.rigol = device
+                    print(f"Found Rigol as {name}\nAt address {address}")
+                    break
             else:
-                rm.close(address)
+                print(f"No USB devices are present")
         if not self.rigol:
-            print(f"Rigol is not present on your system, check connection. It should be plugged in to one of the USB ports.")
-            
+             print(f"Rigol is not present on your system, check connection. It should be plugged in to one of the USB ports.")
+                
