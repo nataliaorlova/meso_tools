cad62bf354fa08666e51e194749d005a29f5ca2b
Natalia <nataliao@alleninstitute.org>
2023-01-11 15:09:44 -0800

    typos

--- meso_tools/laser_mon.py
+++ meso_tools/laser_mon.py
@@ -31,49 +31,47 @@ class RigolAPI():
         self.voltoffset = float(self.scope.query(":CHAN1:OFFS?"))
 
 
-        def get_traces(self, channel : str) -> np.array:
-            """
-                Function to get data fomr the oscillsocope's memory
-            Args:
-                channel (str): channel to connect to, CHAN1 or CHAN2
+    def get_trace(self, channel : str) -> np.array:
+        """
+            Function to get data fomr the oscillsocope's memory
+        Args:
+            channel (str): channel to connect to, CHAN1 or CHAN2
 
-            Returns:
-                np.array: array of datapoint
-            """
-            self.scope.write(":WAV:MODE MAX")
-            self.scope.write(":STOP")
-            self.scope.write(":WAV:FORM ASCii")
-            self.scope.write(f":WAV:SOUR {channel}")
-            self.scope.write(":WAV:POIN 10000")
-            rawdata = self.scope.query(":WAV:DATA?")
-            params = self.scope.query(":WAV:PRE?")
-            self.scope.write(":RUN")
-            self.sample_rate = float(self.scope.query(':ACQ:SRAT?'))
-            params = params.split(',')
-            rawdata=rawdata[11:]
-            data_string = rawdata.split(",")
-            del data_string[-1] # removing new line character
-            self.data = [float(item) for item in data_string]
+        Returns:
+            np.array: array of datapoint
+        """
+        self.scope.write(":WAV:MODE MAX")
+        self.scope.write(":STOP")
+        self.scope.write(":WAV:FORM ASCii")
+        self.scope.write(f":WAV:SOUR {channel}")
+        self.scope.write(":WAV:POIN 10000")
+        rawdata = self.scope.query(":WAV:DATA?")
+        params = self.scope.query(":WAV:PRE?")
+        self.scope.write(":RUN")
+        self.sample_rate = float(self.scope.query(':ACQ:SRAT?'))
+        params = params.split(',')
+        rawdata=rawdata[11:]
+        data_string = rawdata.split(",")
+        del data_string[-1] # removing new line character
+        self.data = [float(item) for item in data_string]
 
 
-        def plot_data(self) -> pl.figure:
-            """
-                Function to generate a figure visualizing the data
-            Args:
-                data (np.array): array of datapoints, units = volts
+    def plot_data(self) -> pl.figure:
+        """
+            Function to generate a figure visualizing the data
+        Args:
+            data (np.array): array of datapoints, units = volts
 
-            Returns:
-                pl.figure: handle to a matplotlib figure
-            """
-            
-            
-            data = np.array(self.data)
-            total_time = len(data)/self.sample_rate / self.timescale  
-            time = np.linspace(0,total_time,num=len(data))
-            # Plot the data
-            fig = pl.figure(figsize=[10, 2])
-            pl.plot(time, data)
-            pl.title("Oscilloscope Channel 1")
-            pl.ylabel("Voltage (V)")
-            pl.xlabel("Time ( ns )")
-            pl.show()
+        Returns:
+            pl.figure: handle to a matplotlib figure
+        """
+        data = np.array(self.data)
+        total_time = len(data)/self.sample_rate / self.timescale  
+        time = np.linspace(0,total_time,num=len(data))
+        # Plot the data
+        fig = pl.figure(figsize=[10, 2])
+        pl.plot(time, data)
+        pl.title("Oscilloscope Channel 1")
+        pl.ylabel("Voltage (V)")
+        pl.xlabel("Time ( ns )")
+        pl.show()
